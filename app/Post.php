82aa3ec137c07cee7f02d22c366cf97ae3e12997
Slamet sugandi <packercyber@gmail.com>
2016-12-01 12:00:09 +0700

    display tumb_image

--- app/Post.php
+++ app/Post.php
@@ -33,6 +33,21 @@ class Post extends Model
     	return $imageUrl;
     }
 
+    public function getImageTumbUrlAttribute($value)
+    {
+        $imageUrl = "";
+
+        if ( ! is_null($this->image))
+        {
+            $ext = substr(strrchr($this->image, '.'), 1);
+            $tumbnail = str_replace(".{$ext}", "_thumb.{$ext}", $this->image);
+            $imagePath = public_path() . "/img/" . $tumbnail;
+            if (file_exists($imagePath)) $imageUrl = asset("img/" . $tumbnail);
+        }
+
+        return $imageUrl;
+    }
+
     public function getDateAttribute($value)
     {
         return is_null($this->published_at) ? '' : $this->published_at->diffForHumans();
@@ -50,7 +65,12 @@ class Post extends Model
 
     public function scopeLatestFirst($query)
     {
-        return $query->orderBy('created_at', 'desc');
+        return $query->orderBy('published_at', 'desc');
+    }
+
+    public function scopePopular($query)
+    {
+        return $query->orderBy('view_count', 'desc');
     }
 
     public function scopePublished($query)
