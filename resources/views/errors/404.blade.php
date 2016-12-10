6c38e1ccc64c1f79dd18e7d2ea653e362ff1d6a6
Slamet Sugandi <packercyber@gmail.com>
2016-12-11 01:15:51 +0700

    penambahan page not found

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+@section('content')
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 hal-not-found">
+				<h2>Halaman tidak tersedia.</h2>
+				<p>maaf!.....Halaman yang ada maskud tidak tersedia</p>
+			</div>
+		</div>
+	</div>
+@endsection
